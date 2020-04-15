583020c1d7c795d27679fc0cd862e9471ecb0f04
dominik59 <osemka59@gmail.com>
2020-04-15 18:46:26 +0200

    [ENV] Improve start_dev_environment.sh script. Make sure that composer dependencies are up to date during environment start

--- scripts/start_dev_environment.sh
+++ scripts/start_dev_environment.sh
@@ -4,4 +4,5 @@ script_dir=$(realpath "$(dirname "$0")")
 main_project_dir=${script_dir}/..
 
 cd "${main_project_dir}" || exit
+"${script_dir}"/api_composer_install.sh
 env UID=$UID GID=$GID docker-compose -f docker-compose-dev.yml up --build --force-recreate
